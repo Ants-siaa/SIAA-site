9e6351ea66ef96f40eb5f60c4506fa4eb85c7789
Ants-siaa <anthony@siaa.org.au>
2026-06-29 20:38:11 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
             <Image
   src="/images/Blue microchip with Australia map.png"
   alt="SIAA Logo"
-  width={40}
-  height={40}
+  width={60}
+  height={60}
   priority
 />
             <div>
