698609085eec91f673c35435c944be203d3f4dd1
Ants-siaa <anthony@siaa.org.au>
2026-07-18 21:10:09 +1000

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -240,7 +240,7 @@ className={`transition-transform ${
 
             <Link
             href="/join"
-            className="rounded-lg bg-[#0066FF] px-5 py-0.5 font-medium text-white shadow hover:bg-blue-700 transition"
+            className="rounded-lg bg-[#0066FF] px-8 py-0.5 font-medium text-white shadow hover:bg-blue-700 transition"
 >
     Join SIAA
 </Link>
